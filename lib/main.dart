bee5e7741dd5ddcd1552b586c4d8f5bb5529ae9f
Phong <phong2k4zero@gmail.com>
2026-05-16 16:35:49 +0700

    chore: add firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:app/core/app_log.dart';
 import 'package:app/feature/cubit/user_cubit.dart';
 import 'package:app/feature/utils/is_login.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -36,7 +37,6 @@ Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
     debugProfileBuildsEnabled = true;
     // debugPaintSizeEnabled = true;
-
     await di.init(); // Chờ inject dependencies
     disableErrorWidget();
     HttpOverrides.global = CustomHttpOverrides();
@@ -66,6 +66,11 @@ Future<void> main() async {
   }, (error, stackTrace) {
     dlog('❎ ERROR OTHER   :$error');
     dlog('❎ STACKTRACE    :$stackTrace');
+    FirebaseCrashlytics.instance.recordError(
+      error,
+      stackTrace,
+      fatal: true,
+    );
   });
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import app_links
 import connectivity_plus
 import file_selector_macos
 import firebase_core
+import firebase_crashlytics
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import path_provider_foundation
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
